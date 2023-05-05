ae8015544f73d94d2856b4aa47665df4b7b20374
Gabriela <gdemelo.mendes@gmail.com>
2023-05-05 17:49:11 -0300

    adding some style configurations
    Added custom colors and font to tailwind config file
    Added font to index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "very-dark-blue": "#161722",
+        "very-dark-desaturated-blue": "#25273C",
+        "light-grayish-blue": "#CACDE8",
+        "light-grayish-blue-hover": "hsl(236, 33%, 92%)",
+        "dark-grayish-blue": "#E4E5F1",
+        "very-dark-grayish-blue": "#4D5066",
+        "very-dark-bluish-gray": "#393A4C",
+      },
+      fontFamily: {
+        "josefin-sans": ["'Josefin Sans', sans-serif"]
+      },
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
 };
