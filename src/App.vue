62a04b8e5aab8600d371ced93eef41e687aad7a7
Gabriela <gdemelo.mendes@gmail.com>
2023-05-06 15:04:38 -0300

    redoing background image loading
    Removed img tag
    Added a div with bg-image and ajusted classes

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,13 @@
 <template>
   <div class="bg-gray-100 min-w-screen min-h-screen relative">
     <!-- Background Picture -->
-    <picture>
-      <source
-        media="(min-width:640px)"
-        srcset="@/assets/images/bg-desktop-light.jpg"
-        class="w-full h-auto object-cover absolute"
-      />
-      <img
-        src="@/assets/images/bg-mobile-light.jpg"
-        class="w-full h-auto object-cover absolute"
-      />
-    </picture>
+    <div
+      class="bg-[url('src/assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-top bg-cover w-full h-72 md:bg-[url('src/assets/images/bg-desktop-light.jpg')] md:h-80"
+    >
+      <!-- App Content -->
+      <div class="container">
+        TESTE
+      </div>
+    </div>
   </div>
 </template>
