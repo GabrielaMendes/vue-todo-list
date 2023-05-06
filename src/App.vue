d5f9c4bf2ed5071f60ab67c7225f3b9958dbebf3
Gabriela <gdemelo.mendes@gmail.com>
2023-05-06 11:17:01 -0300

    setting the background
    Changed background color and added background image
    Different images load depending on screen size

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <header>
-
-  </header>
-
-  <main>
-
-  </main>
+  <div class="bg-gray-100 min-w-screen min-h-screen relative">
+    <!-- Background Picture -->
+    <picture>
+      <source
+        media="(min-width:640px)"
+        srcset="@/assets/images/bg-desktop-light.jpg"
+        class="w-full h-auto object-cover absolute"
+      />
+      <img
+        src="@/assets/images/bg-mobile-light.jpg"
+        class="w-full h-auto object-cover absolute"
+      />
+    </picture>
+  </div>
 </template>
