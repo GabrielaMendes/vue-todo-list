b417962353dfd59b732f141996d78b22af84770f
Gabriela <gdemelo.mendes@gmail.com>
2023-05-06 16:00:08 -0300

    adding the header
    Created new component for the header
    Added logo and theme icon to it

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import TheHeader from "@/components/TheHeader.vue";
+</script>
 
 <template>
   <div class="bg-gray-100 min-w-screen min-h-screen relative">
@@ -7,8 +9,9 @@
       class="bg-[url('src/assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-top bg-cover w-full h-72 md:bg-[url('src/assets/images/bg-desktop-light.jpg')] md:h-80"
     >
       <!-- App Content -->
-      <div class="container">
-        TESTE
+      <div class="container pt-16 px-8 max-w-4xl md:pt-24">
+        <!-- App Header -->
+        <TheHeader />
       </div>
     </div>
   </div>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <header class="flex items-start justify-between">
+    <h1
+      class="font-josefin-sans font-bold text-5xl text-white tracking-[.3em] md:text-6xl"
+    >
+      TODO
+    </h1>
+    <button type="button">
+      <img
+        src="src/assets/images/icon-moon.svg"
+        alt="moon icon"
+        class="h-9 md:h-11"
+      />
+    </button>
+  </header>
+</template>
