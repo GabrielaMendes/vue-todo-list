afbb0617a7946f11c18fb16e235fe243ad276411
Gabriela <gdemelo.mendes@gmail.com>
2023-05-08 11:33:21 -0300

    making the form functional
    Created dumb todo list
    Added ability to add nwe todo to the list
    Added error message in case of invalid todo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <script setup>
+import { computed, ref } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
+
+const newTodo = ref("");
+const validTodo = computed(() => {
+  return newTodo.value.trim().length >= 3
+})
+const errorMessage = ref(false);
+
+const addTodo = () => {
+  if (!validTodo.value) {
+    errorMessage.value = true;
+    return;
+  }
+
+  todoList.value.push(newTodo.value);
+  newTodo.value = "";
+  errorMessage.value = false;
+};
+
+// dumb data
+const todoList = ref([
+  "take out trash",
+  "study vuejs",
+  "go grocery shopping",
+  "update profile picture",
+]);
 </script>
 
 <template>
@@ -15,10 +41,40 @@ import TheHeader from "@/components/TheHeader.vue";
 
         <main>
           <!-- New Todo -->
-          <form @submit="addTodo" class="flex items-start bg-white rounded-md px-7 py-6">
-            <input type="checkbox" name="checkbox" id="checkbox" disabled class="appearance-none h-6 w-6 border-[1.5px] border-gray-200 rounded-full mr-4">
-            <input type="text" placeholder="Create a new todo..." class="focus:outline-none">
+          <form
+            @submit.prevent="addTodo"
+            class="flex items-start bg-white rounded-md px-7 py-6 relative"
+          >
+            <input
+              type="checkbox"
+              name="checkbox"
+              id="checkbox"
+              disabled
+              class="appearance-none h-6 w-6 border-[1.5px] border-gray-200 rounded-full mr-4"
+            />
+            <div>
+              <label for="newTodo" class="hidden">New Todo:</label>
+              <input
+                id="newTodo"
+                type="text"
+                placeholder="Create a new todo..."
+                v-model="newTodo"
+                class="focus:outline-none"
+                @blur="errorMessage = false"
+              />
+                <!-- Error Message -->
+                <p
+                  v-if="errorMessage && !validTodo"
+                  class="text-red-500 text-base"
+                >
+                  Please enter a valid todo!
+                </p>
+            </div>
           </form>
+
+          <p v-for="(todo, i) in todoList" :key="i">
+            {{ todo }}
+          </p>
         </main>
       </div>
     </div>
