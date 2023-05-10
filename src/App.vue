d69a22f86c2a0ff996ee04d427e592d3938f46a0
Gabriela <gdemelo.mendes@gmail.com>
2023-05-10 09:13:21 -0300

    adding bottom section
    Created filter buttons
    Created 'clear completed' button and made it functional

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
 import NewTodo from "@/components/NewTodo.vue";
 import TodoItem from "@/components/TodoItem.vue";
-
-const addTodo = (newTodoText) => {
-  todoList.value.push({
-    id: crypto.randomUUID(),
-    text: newTodoText,
-    completed: false,
-  });
-};
-
-const removeTodo = (id) => {
-  todoList.value = todoList.value.filter((todo) => todo.id !== id);
-};
-
-const toggleCompleted = (id) => {
-  const todo = todoList.value.find((todo) =>  todo.id === id)
-  todo.completed = !todo.completed;
-}
 
 // dumb data
 const todoList = ref([
@@ -44,6 +27,43 @@ const todoList = ref([
     completed: false,
   },
 ]);
+
+const addTodo = (newTodoText) => {
+  todoList.value.push({
+    id: crypto.randomUUID(),
+    text: newTodoText,
+    completed: false,
+  });
+};
+
+const removeTodo = (id) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== id);
+};
+
+const clearCompleted = () => {
+  todoList.value.forEach((todo) => {
+    todo.completed = false;
+  });
+};
+
+const toggleCompleted = (id) => {
+  const todo = todoList.value.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+};
+
+const todosLeft = computed(() => {
+  return todoList.value.filter((todo) => !todo.completed);
+});
+
+const todosCompleted = computed(() => {
+  return todoList.value.filter((todo) => todo.completed);
+});
+
+const filter = ref("All");
+
+const changeFilter = (option) => {
+  filter.value = option;
+};
 </script>
 
 <template>
@@ -64,9 +84,14 @@ const todoList = ref([
           <!-- Todo List -->
           <div class="mt-5 rounded-md bg-white">
             <div v-if="todoList.length === 0" class="text-center px-7 py-6">
-              <h3 class="mt-2 mb-3 text-3xl text-transparent font-bold bg-gradient-to-br from-light-blue to-strong-purple-pink bg-clip-text">No todos here</h3>
+              <h3
+                class="mt-2 mb-3 text-3xl text-transparent font-bold bg-gradient-to-br from-light-blue to-strong-purple-pink bg-clip-text"
+              >
+                No todos here
+              </h3>
               <p class="text-base">Add a new todo to start</p>
             </div>
+
             <TodoItem
               v-for="todo in todoList"
               :key="todo.id"
@@ -74,6 +99,56 @@ const todoList = ref([
               @remove-todo="removeTodo"
               @toggle-completed="toggleCompleted"
             />
+
+            <!-- Summary, Filters and Clear -->
+            <div class="text-dark-grayish-blue flex justify-between px-7 py-6">
+              <p class="text-base">{{ todosLeft.length }} items left</p>
+              <ul class="hidden md:flex gap-5 md:relative md:left-6">
+                <li
+                  role="button"
+                  class="hover:text-very-dark-grayish-blue"
+                  :class="filter === 'All' ? 'text-blue-500 pointer-events-none' : ''"
+                >
+                  All
+                </li>
+                <li
+                  role="button"
+                  class="hover:text-very-dark-grayish-blue"
+                  :class="filter === 'Active' ? 'text-blue-500 pointer-events-none' : ''"
+                >
+                  Active
+                </li>
+                <li
+                  role="button"
+                  class="hover:text-very-dark-grayish-blue"
+                  :class="filter === 'Completed' ? 'text-blue-500 pointer-events-none' : ''"
+                >
+                  Completed
+                </li>
+              </ul>
+              <button
+                type="button"
+                @click.prevent="clearCompleted"
+                class="text-base hover:text-very-dark-grayish-blue"
+              >
+                Clear Completed
+              </button>
+            </div>
+          </div>
+
+          <!-- Filters Mobile -->
+          <div class="text-dark-grayish-blue mt-5 rounded-md bg-white">
+            <ul class="px-7 py-6 justify-center flex gap-5">
+              <li role="button" class="hover:text-very-dark-grayish-blue">
+                All
+              </li>
+              <li role="button" class="hover:text-very-dark-grayish-blue">
+                Active
+              </li>
+              <li role="button" class="hover:text-very-dark-grayish-blue">
+                Completed
+              </li>
+            </ul>
           </div>
         </main>
       </div>
